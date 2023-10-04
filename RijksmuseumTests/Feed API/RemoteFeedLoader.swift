3e217a2c6cabe079dcba8530d9e27dc5cb29368f
Andrii Mamchenko <atmamont@gmail.com>
2023-10-04 10:26:50 +0200

    Create `RemoteFeedLoader` implementation of `FeedLoader` interface. Check it doesn't trigger load upon instatiation

---
+++ RijksmuseumTests/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,41 @@
+//
+//  RemoteFeedLoader.swift
+//  RijksmuseumTests
+//
+//  Created by Andrei on 03/10/2023.
+//
+
+import XCTest
+import Rijksmuseum
+
+class RemoteFeedLoader {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        
+    }
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotTriggerServiceRequest() {
+        let client = URLSessionHTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.getCallCount, 0)
+    }
+    
+    private class URLSessionHTTPClientSpy: HTTPClient {
+        var getCallCount = 0
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            getCallCount += 1
+        }
+    }
+}
